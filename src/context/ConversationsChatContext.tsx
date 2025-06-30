8a1c9e7fc3335cca7dd12ff4d1e710b97f063929
danielcalado07 <daniel.calado.profissional@gmail.com>
2025-06-30 13:14:21 -0300

    feat: adicionar contexto para as conversas

---
+++ src/context/ConversationsChatContext.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Conversation, Message } from "@/types/message";
+import { storage } from "@/utils/useLocalStorage";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ConversationsChatContextType {
+  conversationsChats: Conversation[];
+  setConversationsChats: (chats: Conversation[]) => void;
+  selectedChat: Message[] | null;
+  setSelectedChat: (chat: Message[] | null) => void;
+}
+
+const ConversationsChatContext = createContext<
+  ConversationsChatContextType | undefined
+>(undefined);
+
+interface ConversationsChatProviderProps {
+  children: React.ReactNode;
+}
+
+export function ConversationsChatProvider({
+  children,
+}: ConversationsChatProviderProps) {
+  const [conversationsChats, setConversationsChatsState] = useState<
+    Conversation[]
+  >([]);
+  const [selectedChat, setSelectedChatState] = useState<Message[] | null>(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    const storedChats = storage.getItem("conversationsChats");
+    const storedSelectedChat = storage.getItem("selectedChat");
+
+    if (storedChats) setConversationsChatsState(JSON.parse(storedChats));
+    if (storedSelectedChat)
+      setSelectedChatState(JSON.parse(storedSelectedChat));
+
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      storage.setItem("conversationsChats", JSON.stringify(conversationsChats));
+    }
+  }, [conversationsChats, isClient]);
+
+  useEffect(() => {
+    if (isClient) {
+      storage.setItem("selectedChat", JSON.stringify(selectedChat));
+    }
+  }, [selectedChat, isClient]);
+
+  const setConversationsChats = (chats: Conversation[]) => {
+    setConversationsChatsState(chats);
+  };
+
+  const setSelectedChat = (chat: Message[] | null) => {
+    setSelectedChatState(chat);
+  };
+
+  return (
+    <ConversationsChatContext.Provider
+      value={{
+        conversationsChats,
+        setConversationsChats,
+        selectedChat,
+        setSelectedChat,
+      }}
+    >
+      {children}
+    </ConversationsChatContext.Provider>
+  );
+}
+
+export function useConversationsChat() {
+  const context = useContext(ConversationsChatContext);
+  if (!context) {
+    throw new Error(
+      "useConversationsChat must be used within a ConversationsChatProvider",
+    );
+  }
+  return context;
+}
